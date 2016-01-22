6903a1764d73d54bbbd516c9935eba7982e4911a
Joshua Jenkins <josh@joshuajenkins.com>
2016-01-21 20:06:43 -0800

    Short name for url purposes and stubbed entry function

--- cdec.js
+++ cdec.js
@@ -7,28 +7,112 @@ var validStations = ['CLE', 'SHA', 'ORO', 'FOL', 'SNL', 'NML', 'EXC', 'DNP', 'MI
 
 // sensors that supply daily historic data
 // sensors that supply only hourly, weekly, monthly, or event data are omitted
-var dailySensorCodes = [
-  {id: 2,   name: "Precipitation, accumulated",       unit: "in"  },
-  {id: 6,   name: "Reservoir elevation",              unit: "ft"  },
-  {id: 8,   name: "Full natural flow",                unit: "cfs" },
-  {id: 15,  name: "Reservoir storage",                unit: "af"  },
-  {id: 22,  name: "Reservoir, storage change",        unit: "af"  },
-  {id: 23,  name: "Reservoir outflow",                unit: "cfs" },
-  {id: 30,  name: "Temperature, air average"          unit: "degf"},
-  {id: 31,  name: "Temperature, air maximum",         unit: "degf"},
-  {id: 32,  name: "Temperature, air minimum",         unit: "degf"},
-  {id: 45,  name: "Precipitation, incremental",       unit: "in"  },
-  {id: 64,  name: "Evaporation, pan increment",       unit: "in"  },
-  {id: 48,  name: "Discharge, power generation",      unit: "cfs" },
-  {id: 70,  name: "Discharge, pumping",               unit: "cfs" },
-  {id: 71,  name: "Discharge, spillway",              unit: "cfs" },
-  {id: 74,  name: "Evaporation, Lake computed",       unit: "cfs" },
-  {id: 76,  name: "Reservoir inflow",                 unit: "cfs" },
-  {id: 85,  name: "Discharge, control regulating",    unit: "cfs" },
-  {id: 93,  name: "Wind, travel length"               unit: "mi"  },
-  {id: 94,  name: "Reservoir, top conserv storage",   unit: "af"  },
-  {id: 200, name: "Res, ABV top of conserv storage",  unit: "af"  },
-  {id: 203, name: "Precipitation, incrmnt 4A-4A",     unit: "in"  }
+var dailySensors = [
+  { id:         2,
+    shortName:  "precip-accumulated",
+    name:       "Precipitation, accumulated",
+    unit:       "in"
+  },
+  { id:         6,
+    shortName:  "reservoir-elevation",
+    name:       "Reservoir elevation",
+    unit:       "ft"
+  },
+  { id:         8,
+    shortName:  "full-natural-flow",
+    name:       "Full natural flow",
+    unit:       "cfs"
+  },
+  { id:         15, 
+    shortName:  "reservoir-storage",
+    name:       "Reservoir storage",
+    unit:       "af"
+  },
+  { id:         22,
+    shortName:  "reservoir-storage-change",
+    name:       "Reservoir, storage change",
+    unit:       "af"
+  },
+  { id:         23,
+    shortName:  "reservoir-outflow",
+    name:       "Reservoir outflow",
+    unit:       "cfs"
+  },
+  { id:         30,
+    shortName:  "avg-temp",
+    name:       "Temperature, air average",
+    unit:       "degf"
+  },
+  { id:         31,
+    shortName:  "max-temp",
+    name:       "Temperature, air maximum",
+    unit:       "degf"
+  },
+  { id:         32,
+    shortName:  "min-temp",
+    name:       "Temperature, air minimum",
+    unit:       "degf"
+  },
+  { id:         45,
+    shortName:  "precip-incremental",
+    name:       "Precipitation, incremental",
+    unit:       "in"
+  },
+  { id:         64,
+    shortName:  "evaporation-pan-increment",
+    name:       "Evaporation, pan increment",
+    unit:       "in"
+  },
+  { id:         48,
+    shortName:  "discharge-power-gen",
+    name:       "Discharge, power generation",
+    unit:       "cfs"
+  },
+  { id:         70,
+    shortName:  "discharge-pumping",
+    name:       "Discharge, pumping",
+    unit:       "cfs"
+  },
+  { id:         71,
+    shortName:  "discharge-spillway",
+    name:       "Discharge, spillway",
+    unit:       "cfs"
+  },
+  { id:         74,
+    shortName:  "evaporation-lake-computed",
+    name:       "Evaporation, Lake computed",
+    unit:       "cfs"
+  },
+  { id:         76,
+    shortName:  "reservoir-inflow",
+    name:       "Reservoir inflow",
+    unit:       "cfs"
+  },
+  { id:         85,
+    shortName:  "discharge-control-regulating",
+    name:       "Discharge, control regulating",
+    unit:       "cfs"
+  },
+  { id:         93,
+    shortName:  "wind-travel-length",
+    name:       "Wind, travel length",
+    unit:       "mi"
+  },
+  { id:         94,
+    shortName:  "reservoir-top-conserv-storage",
+    name:       "Reservoir, top conserv storage",
+    unit:       "af"
+  },
+  { id:         200,
+    shortName:  "reservoir-abv-top-conserv-storage",
+    name:       "Res, ABV top of conserv storage",
+    unit:       "af"
+  },
+  { id:         203,
+    shortName:  "precip-incremental-4a-4a",
+    name:       "Precipitation, incrmnt 4A-4A",
+    unit:       "in"
+  }
 ]
 
 var stationDailySensors = {
@@ -44,6 +128,12 @@ var stationDailySensors = {
   PNF: [  6,8,15,22,23,   31,32,45,   64,         76,   93,94,200,203],
   CAS: [  6,  15,22,                                                 ],
   PRR: [  6,  15,22,            45                                   ]
+}
+
+var handler = function(req, res) {
+  // parse params
+  // call func
+  // render resp
 }
 
 var isValidStation = function(station) {
